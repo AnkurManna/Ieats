3fc7e31d4915dd00e77109da9e8a1a368bf262f0
Ankur Manna <ankurcasino108@gmail.com>
2021-05-22 09:21:51 +0530

    package structure(#1)

---
+++ Ieats/src/main/java/Ieats/IeatsApplication.java
@@ -0,0 +1,13 @@
+package Ieats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeatsApplication.class, args);
+	}
+
+}
---
+++ Ieats/src/test/java/Ieats/IeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package Ieats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
