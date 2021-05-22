af1bdf87eb1502ea3c1d1686a90086100f28fdda
Ankur Manna <ankurcasino108@gmail.com>
2021-05-22 11:42:11 +0530

    unit testing for user

--- Ieats/src/test/java/Ieats/IeatsApplicationTests.java
+++ Ieats/src/test/java/Ieats/IeatsApplicationTests.java
@@ -1,13 +1,47 @@
 package Ieats;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import Ieats.domainmodel.models.User;
+import Ieats.service.accessoperation.UserAccessOperation;
+import Ieats.service.repository.UserRepository;
+import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertEquals;
 @SpringBootTest
 class IeatsApplicationTests {
 
+	@Autowired
+	private UserAccessOperation operation;
+	
+	@MockBean
+	private UserRepository repository;
+	
 	@Test
-	void contextLoads() {
+	public void getUsersByMailTest()
+	{
+		when(repository.findByMail("lll")).thenReturn(
+				Optional.of(new User("name","password","gender","mail"))
+				);
+		assertEquals("name",operation.findByMail("lll").get().getName());
 	}
+	
+	@Test
+	public void getUsersByIdTest()
+	{
+		when(repository.findById("lll")).thenReturn(
+				Optional.of(new User("name","password","gender","mail"))
+				);
+		assertEquals("name",operation.findById("lll").get().getName());
+	}
+
+	
 
 }
