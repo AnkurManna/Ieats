a5d3d83ea57e460cfb788281894f8ae9bb4d4b4c
Ankur Manna <ankurcasino108@gmail.com>
2021-06-05 20:39:08 +0530

    session config

---
+++ Ieats/src/main/java/Ieats/web/config/SessionConfig.java
@@ -0,0 +1,15 @@
+package Ieats.web.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+//import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+//import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+
+
+public class SessionConfig
+ {
+
+   
+}
